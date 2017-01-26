707ad50367c6a3b1f6633397a628f4aad1b1f99e
aarondevon <me@aarondevon.com>
2017-01-26 17:47:37 -0600

    run and test player page 15

---
+++ EscapeFromChateauDeLaMisererieSawyerGroesbeckMcEwan/src/model/Player.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author aaronsawyer
+ */
+public class Player implements Serializable {
+    private String name;
+    private double bestTime;
+    private List inventroy;
+
+    public Player() {
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBestTime() {
+        return bestTime;
+    }
+
+    public void setBestTime(double bestTime) {
+        this.bestTime = bestTime;
+    }
+
+    public List getInventroy() {
+        return inventroy;
+    }
+
+    public void setInventroy(List inventroy) {
+        this.inventroy = inventroy;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.name);
+        hash = 17 * hash + (int) (Double.doubleToLongBits(this.bestTime) ^ (Double.doubleToLongBits(this.bestTime) >>> 32));
+        hash = 17 * hash + Objects.hashCode(this.inventroy);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", bestTime=" + bestTime + ", inventroy=" + inventroy + '}';
+    }
+    
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.inventroy, other.inventroy)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+}
