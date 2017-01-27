8e160eea6cc1aba0dbe4fb0d09082e5e4894f8b1
Sudo <mcewantl@gmail.com>
2017-01-27 12:32:37 -0700

    Warden added but not finished
    Signed-off-by: Sudo <mcewantl@gmail.com>

---
+++ EscapeFromChateauDeLaMisererieSawyerGroesbeckMcEwan/src/model/Warden.java
@@ -0,0 +1,31 @@
+
+package model;
+
+/**
+ *
+ * @author Tim McEwan
+ */
+public class Warden {
+   private String Name;
+   private String  Decription;
+
+    public Warden() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getDecription() {
+        return Decription;
+    }
+
+    public void setDecription(String Decription) {
+        this.Decription = Decription;
+    }
+  
+ }
