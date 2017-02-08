a7df37d1bf0ff7ac5a1e219288a8692ed5c9ac3b
aarondevon <me@aarondevon.com>
2017-02-07 22:04:09 -0600

    start work on requiredKeys method

---
+++ EscapeFromChateauDeLaMisererieSawyerGroesbeckMcEwan/src/control/ItemControl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+/**
+ *
+ * @author aaronsawyer
+ */
+public class ItemControl {
+
+    public boolean requiredKeys(int itemInventory[], int location) {
+        if (itemInventory[location - 1 != null]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+}
