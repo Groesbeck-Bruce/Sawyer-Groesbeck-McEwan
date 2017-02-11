8eb7b13d9b91cafc46b7770219e6ac311adbe292
bruce <bruce@DESKTOP-N91RPL9>
2017-02-11 09:53:20 -0800

    Test for LogicControl

---
+++ EscapeFromChateauDeLaMisererieSawyerGroesbeckMcEwan/src/control/LogicControl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+/**
+ *
+ * @author bruce
+ */
+public class LogicControl {
+
+    public double stoneAreaChallenge(int height, int width, int length) {
+        double[] stoneAreaChallenge = null;
+        return stoneAreaChallenge[height + width + length];
+
+    }
+
+    public double liquidChallenge(int height, int width, int length) {
+        int[] liquidChallenge = null;
+        return liquidChallenge [height + width + length];
+
+    }
+
+   
+    public double armoryLockChallenge(double sideA, double sideB, double sideC){
+        double result = sideA + sideB + sideC;    
+        return result;
+    }
+
+}
---
+++ EscapeFromChateauDeLaMisererieSawyerGroesbeckMcEwan/test/control/LogicControlTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author bruce
+ */
+public class LogicControlTest {
+    
+    public LogicControlTest() {
+    }
+
+    /**
+     * Test of stoneAreaChallenge method, of class LogicControl.
+     */
+    @Test
+    public void testStoneAreaChallenge() {
+        System.out.println("stoneAreaChallenge Test 1");
+        double height = 0.0;
+        double width = 0.0;
+        double length = 0.0;
+        LogicControl instance = new LogicControl();
+        double expResult = 0.0;
+        //double result = instance.stoneAreaChallenge(height, width, length);
+        //assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of liquidChallenge method, of class LogicControl.
+     */
+    @Test
+    public void testLiquidChallenge() {
+        System.out.println("liquidChallenge Test 1");
+        int height = 0;
+        int width = 0;
+        int length = 0;
+        LogicControl instance = new LogicControl();
+        double expResult = 0.0;
+        //double result = instance.liquidChallenge(height, width, length);
+        //assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of armoryLockChallenge method, of class LogicControl.
+     */
+    @Test
+    public void testArmoryLockChallenge() {
+        System.out.println("Armory Lock Challenge Test 1");
+        double sideA = 4.1;
+        double sideB = 4.2;
+        double sideC = 4.3;
+        LogicControl instance = new LogicControl();
+        double expResult = 12.6;
+        //assertArrayEquals(expResult,result);
+        // TODO review the generated test code and remove the default call to fail.
+           }
+    
+}
