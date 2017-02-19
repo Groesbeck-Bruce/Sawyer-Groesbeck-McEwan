afc880a8277aaed07857739fd39a2d945137f08d
Tim McEwan <>
2017-02-18 21:53:45 -0700

    On page beginning 44

--- EscapeFromChateauDeLaMisererieSawyerGroesbeckMcEwan/src/EscapeFromChateauDeLaMisererie/view/MainMenuView.java
+++ EscapeFromChateauDeLaMisererieSawyerGroesbeckMcEwan/src/EscapeFromChateauDeLaMisererie/view/MainMenuView.java
@@ -10,9 +10,42 @@ package EscapeFromChateauDeLaMisererie.view;
  * @author sudo
  */
 public class MainMenuView {
+  private String menu;
+  
+  public MainMenuView() {
+  this.menu = "\n"
+            + "\n------------------------------"
+            + "\n | Main Menu                 |"
+            + "\n------------------------------"
+            + "\nN - Start Game"
+            + "\nG - Get and start saved game"
+            + "\nH - Get help you noob"
+            + "\nS - Save Game"
+            + "\nQ - Quit Game"
+            + "\n------------------------------";
 
+          }
     void displayMainMenueView() {
-        System.out.println("DisplayMenue()");
+      
+        boolean done = false;
+        do {
+               String menuOption = this.getMenuOption();
+               if (menuOption.toUpperCase().equals("Q"))
+                   return;
+               
+               done = this.doAction(menuOption);
+        } while (!done);
     }
+    private String getMenuOption() {
+        System.out.println("\n**** getMenuOption() fired");
+        return "N";
+    }
+    private boolean doAction(String menuOption) {
+           System.out.println("\n*** doAction() fired ***");
+           return true;
+    }
+
+
     
+
 }
